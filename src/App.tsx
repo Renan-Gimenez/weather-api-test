566e7e67b08eae79a14f1020c2618cf61110c1a8
Renan Gimenez <renangiimenez@gmail.com>
2023-02-20 17:45:53 -0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// Components
+import Header from './components/Header';
+import Search from './components/Search';
+
+// Icons
+import { MdLocationOn } from 'react-icons/md';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Header>Confira o clima de uma cidade:</Header>
+        <Search />
+        <div className="line"></div>
+        <img src="https://countryflagsapi.com/png/BR" alt="country-icon" />
+      </div>
     </div>
   );
 }
