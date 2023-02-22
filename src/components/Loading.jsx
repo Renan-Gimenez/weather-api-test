bd8880a66acbe9a5222fa17558eb415577a5e1fd
Renan Gimenez <renangiimenez@gmail.com>
2023-02-22 18:08:16 -0300

    add loader component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { BiLoaderAlt } from 'react-icons/bi';
+import { FiLoader } from 'react-icons/fi';
+
+export default function Loading() {
+    return (
+        <>
+            {/* <BiLoaderAlt id="loader" /> */}
+            <FiLoader id="loader"/>
+        </>
+    );
+}
