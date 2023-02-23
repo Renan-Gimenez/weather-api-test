b045dccef1f981ae0adb30a40e8a64690c86f08b
Renan Gimenez <renangiimenez@gmail.com>
2023-02-23 01:37:50 -0300

    error validation

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,9 @@ import Loading from './components/Loading';
 function App() {
   const [cityName, setCityName] = useState('');
   const [name, setName] = useState('');
+
   const [loading, setLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
   const [ready, setReady] = useState(true);
 
   const [countryFlag, setCountryFlag] = useState('');
@@ -29,60 +31,54 @@ function App() {
     const apiKey = '6615acb49bc873de86d687d122155ade';
     const ApiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
     
-    const { data } = await axios.get(ApiWeatherURL);
-
-    return data;
+    try {
+      setLoading(true);
+      setHasError(false);
+      
+      return await axios.get(ApiWeatherURL);
+    } catch (error) {
+      setHasError(true);
+      return {error};
+    } finally {
+      setLoading(false);
+    }
   }
 
   const showWeatherData = async () => {
+    const { data, error } = await getWeatherData(cityName);
 
-    // document.querySelector(".info-container").classList.add("hide");
-    // document.querySelector("#loader").classList.remove("hide");
+    if (!hasError) {
+      setCountryFlag(`https://flagcdn.com/16x12/${data.sys.country}.png`);
+  
+      setName(cityName);
+  
+      setTemperature(parseInt(data.main.temp));
+      setDescription(data.weather[0].description);
+      setWeatherIcon(`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+      setHumidity(data.main.humidity);
+      setWind(data.wind.speed);
+    }
 
-    setLoading(true);
-    
-    const data = await getWeatherData(cityName);
-    console.log(data);
-
-    setCountryFlag(`https://flagcdn.com/16x12/${data.sys.country}.png`);
-
-    // setCityName(data.name);
-    // setCityName(inputValue);
-    setName(cityName);
-    
-    console.log(cityName);
-
-    setTemperature(parseInt(data.main.temp));
-    setDescription(data.weather[0].description);
-    setWeatherIcon(`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
-    setHumidity(data.main.humidity);
-    setWind(data.wind.speed);
-
-    setLoading(false);
     setReady(false);
-    // document.querySelector(".info-container").classList.remove("hide");
-    // document.querySelector("#loader").classList.add("hide");
   }
 
   return (
     <div className="App">
       <div className="main-container">
         <Header>Confira o clima de uma cidade:</Header>
-        {/* <Search setInputValue={setInputValue} inputValue={inputValue} onClick={() => { */}
         <Search setCityName={setCityName} cityName={cityName} onClick={() => { 
           showWeatherData()
         }} onChange={() => {
           setReady(true);
         }} />
         {loading && <Loading />}
+        {!loading && hasError && <p>Cidade não encontrada</p>}
         {!loading && !ready && (
           <div className="info-container">
             <div className="line"></div>
             <div className='city-container'>
               <MdLocationOn />
               <p>{name}</p>
-              {/* <img src="https://countryflagsapi.com/png/BR" /> */}
-              {/* <img src={countryFlag} /> */}
             </div>
             <p id='temp-element'>{temperature} ˚C</p>
             <div className="description-container">
