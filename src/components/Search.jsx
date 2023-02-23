6af7c7237ec7ed2a0348b6506e46e7edb3dd1f5f
Renan Gimenez <renangiimenez@gmail.com>
2023-02-22 23:24:30 -0300

    feat: fix input action

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,28 +2,38 @@ import React from "react";
 import { MdLocationOn } from 'react-icons/md';
 import { BiSearch } from 'react-icons/bi';
 
-// import styled from "styled-components";
-
-// document.querySelector('#input-city').addEventListener("keyup", (e) => {
-//     if (e.code === "Enter") {
-//         const city = e.target.value;
-//     }
-// });
-
 export default function Search({ onClick, onChange, setCityName, cityName }) {
 
     const value = cityName;
     const setValue = setCityName;
 
+    const cityInput = document.querySelector("#input-city");
+
+    const resetInput = () => {
+        const inputCity = document.querySelector("#input-city");
+
+        // inputCity.value = "";
+        inputCity.blur();
+    }
+    
     return (
         <div className="search-container">
-            <input id="input-city" spellCheck="true" onChange={(e) => {
+            <input typeof="password" id="input-city" spellCheck="true" onChange={(e) => {
                 setValue(e.target.value);
                 console.log(value);
 
                 onChange();
 
-            }} value={value} type="text" placeholder="Nome da cidade"/>
+            }} onKeyUp={(e) => {
+                if (e.code === "Enter") {
+                    onClick();
+                }
+
+                if (e.code === "Escape") {
+                    // onClick();
+                    resetInput();
+                }
+            }} value={value} type="text" placeholder="Nome da cidade" />
             <button onClick={onClick}><BiSearch /></button>
         </div>
     );
